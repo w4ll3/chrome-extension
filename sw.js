ac1098ce1e708f459bc0385f9554b24722ee931f
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-02-19 22:29:17 -0300

    working with last credible web

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-importScripts('flutter_service_worker.js');
-
 chrome.runtime.onMessageExternal.addListener((request, sender, senderResponse) => {
   console.log(request, sender, senderResponse);
 })
