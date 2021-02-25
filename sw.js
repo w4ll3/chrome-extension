504522039279cf3ea8efa149065a7bae79a58b4a
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-02-25 12:48:18 -0300

    window popup on install

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
 chrome.runtime.onMessageExternal.addListener((request, sender, senderResponse) => {
   console.log(request, sender, senderResponse);
 })
+
+self.addEventListener("install", () => {
+  chrome.windows.create({
+    focused: true,
+    url: chrome.runtime.getURL('index.html'),
+    width: 338,
+    height: 600,
+    top: 64,
+    left: 999999,
+    type: "popup"
+  })
+ });
